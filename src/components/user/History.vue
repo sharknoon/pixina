f8bd4c8dddd64cd547f5ef476125918f59fc6116
Josua Frank <frank.josua@gmail.com>
2020-10-19 17:05:49 +0200

    Update History.vue

--- src/components/user/History.vue
+++ src/components/user/History.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <img :src="items[current_item]">
+        <img :src="items[current_item].src">
         <form>
             <input type="range" class="form-control-range" min="0" max="99" v-model="current_item">
         </form>
@@ -20,9 +20,11 @@ export default {
       return require("./../../assets/images/history/" + number + ".webp");
     },
   },
-  created() {
+  mounted() {
     for (let index = 0; index < 100; index++) {
-      this.items.push(this.getItemUrl(index));
+      let image = new Image();
+      image.src = this.getItemUrl(index);
+      this.items.push(image);
     }
   },
 };
