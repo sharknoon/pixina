a54ea338c4aa7e90a5c0d810a303c1add628bffc
Josua Frank <frank.josua@gmail.com>
2020-10-19 16:42:00 +0200

    Update History.vue

--- src/components/user/History.vue
+++ src/components/user/History.vue
@@ -1,5 +1,29 @@
 <template>
     <div>
-        History
+        <img :src="items[current_item]">
+        <form>
+            <input type="range" class="form-control-range" min="0" max="99" v-model="current_item">
+        </form>
     </div>
 </template>
+<script>
+export default {
+  name: "History",
+  data() {
+    return {
+      items: [],
+      current_item: 0,
+    };
+  },
+  methods: {
+    getItemUrl(number) {
+      return require("./../../assets/images/history/" + number + ".webp");
+    },
+  },
+  created() {
+    for (let index = 0; index < 100; index++) {
+      this.items.push(this.getItemUrl(index));
+    }
+  },
+};
+</script>
