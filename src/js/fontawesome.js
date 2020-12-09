3f58de8337b5eee413ca14f4e2e7f94ab2af24b1
Josua Frank <josua.frank@diehl-defence.com>
2020-12-09 17:39:39 +0100

    - removed unnecessary tooltip and popover functions - removed unnecessary fontawesome imports

--- src/js/fontawesome.js
+++ src/js/fontawesome.js
@@ -5,18 +5,4 @@
 import Vue from 'vue'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-// import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
-// import { faGoogle } from '@fortawesome/free-brands-svg-icons'
-// import { faApple } from '@fortawesome/free-brands-svg-icons'
-// import { faMicrosoft } from '@fortawesome/free-brands-svg-icons'
-// import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
-
-// library.add(
-//   faEnvelope,
-//   faGoogle,
-//   faApple,
-//   faMicrosoft,
-//   faArrowRight,
-// )
-
 Vue.component('font-awesome-icon', FontAwesomeIcon)
