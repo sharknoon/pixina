6e2d8cea1f8e0ba746052b7a231b79cc616e1393
Josua Frank <josua.frank@daimlertruck.com>
2022-08-10 14:00:59 +0200

    removed cache clearing

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,4 @@ import router from "@/router";
 import i18n from "@/i18n";
 import "@/bootstrap";
 
-caches.keys().then((names) => names.forEach((name) => caches.delete(name)));
-
 createApp(App).use(pinia).use(router).use(i18n).mount("#app");
